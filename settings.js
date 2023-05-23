109d16d097ee9c0cfd1aa2d51fc285a2cb31a76b
plaul <lmor@kea.dk>
2023-05-23 16:28:46 +0200

    server url

--- settings.js
+++ settings.js
@@ -1,1 +1,3 @@
-export const SERVER_API = "http://localhost:8080/api/"
+//export const SERVER_API = "http://localhost:8080/api/"
+export const SERVER_API = "https://grocery.azurewebsites.net/api/"
+
